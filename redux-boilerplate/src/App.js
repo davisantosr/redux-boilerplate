94cb2240eb41a043dd07accf3eb514ceab569108
davisantosr <davi-rocha2@hotmail.com>
2020-12-19 21:35:03 -0300

    add initial directories| store | root reducer

--- redux-boilerplate/src/App.js
+++ redux-boilerplate/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux'
+
+import store from './store'
+
+import Home from './containers/Home/Home'
 
 const App = () => (
-  <div>
-    App
-  </div>
+  
+  <Provider store={store}>
+    <Router>
+      <Route exact path='/' component={Home} />
+    </Router>
+  </Provider>
+  
 )
 
 
---
+++ redux-boilerplate/src/containers/Home/Home.js
@@ -0,0 +1,11 @@
+
+
+const Home = () => (
+  <>
+    <h1>
+      Home
+    </h1>
+  </>
+)
+
+export default Home;
